afaf518c325367188ae14fabecd4e2546d11011e
Ahad Islam <ahad.islam624@gmail.com>
2020-05-02 16:31:09 -0400

    added an api model, when publisher emits value it reloads data.

--- TableviewCombine/API.swift
+++ TableviewCombine/API.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Combine
 
 struct API {
     /// API Errors.
--- TableviewCombine/ViewController.swift
+++ TableviewCombine/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Combine
+import SafariServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var subscriptions = Set<AnyCancellable>()
+        
+    private let stories = CurrentValueSubject<[Story], Never>([])
+            
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadData()
+    }
+    
+    private func loadData() {
+        let api = API()
+        api.stories()
+            .receive(on: DispatchQueue.main)
+        .sink(receiveCompletion: { _ in }, receiveValue: { stories in
+            self.stories.send(stories)
+            self.tableView.reloadData()
+        })
+        .store(in: &subscriptions)
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        stories.value.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "storyCell", for: indexPath)
+        cell.textLabel?.text = stories.value[indexPath.row].title
+        cell.detailTextLabel?.text = stories.value[indexPath.row].by
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let something = SFSafariViewController(url: URL(string: stories.value[indexPath.row].url)!)
+        present(something, animated: true)
+    }
+    
+    
+}
