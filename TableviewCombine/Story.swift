ded80b5338f6488d3976d53225a4de38e4768c11
Ahad Islam <ahad.islam624@gmail.com>
2020-05-02 16:16:30 -0400

    added models for the api and story.

---
+++ TableviewCombine/Story.swift
@@ -0,0 +1,29 @@
+//
+//  Story.swift
+//  TableviewCombine
+//
+//  Created by Ahad Islam on 5/2/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import Foundation
+
+public struct Story: Codable {
+  public let id: Int
+  public let title: String
+  public let by: String
+  public let time: TimeInterval
+  public let url: String
+}
+
+extension Story: Comparable {
+  public static func < (lhs: Story, rhs: Story) -> Bool {
+    return lhs.time > rhs.time
+  }
+}
+
+extension Story: CustomDebugStringConvertible {
+  public var debugDescription: String {
+    return "\n\(title)\nby \(by)\n\(url)\n-----"
+  }
+}
