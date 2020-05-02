dcbbcf2eeab9bddd66cdeab9aebcd64b9caa89b8
Ahad Islam <ahad.islam624@gmail.com>
2020-05-02 16:14:28 -0400

    Initial Commit

---
+++ TableviewCombine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableviewCombine
+//
+//  Created by Ahad Islam on 5/2/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
